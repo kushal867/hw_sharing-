b0d1c490a53b38f5b43dc03ff5922236015de53c
Kushal <kushalsapkota577@gmail.com>
2025-11-12 16:27:45 +0545

    feat:updated

--- hw/forms.py
+++ hw/forms.py
@@ -3,25 +3,6 @@ from django.contrib.auth.models import User
 from .models import Homework, Submission, Comment, Profile
 
 
-# USER REGISTRATION FORM
-
-class UserRegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
-
-        if password and confirm_password and password != confirm_password:
-            raise forms.ValidationError("Passwords do not match.")
-        return cleaned_data
-
 
 #   PROFILE FORM
 
