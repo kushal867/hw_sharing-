335a5a2c5cc4b376a5daed20f4bb29ca3a074bb8
Kushal <kushalsapkota577@gmail.com>
2025-11-12 09:56:45 +0545

    feat:Updated

--- hw/forms.py
+++ hw/forms.py
@@ -7,3 +7,22 @@ class HomeworkForm(forms.ModelForm):
     class Meta:
         model = Homework
         fields = "__all__"
+
+class SubmissionForm(forms.ModelForm):
+    class Meta:
+        model = Submission
+        fields = "__all__"
+
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = "__all__"
