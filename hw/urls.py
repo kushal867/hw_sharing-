6d2d7b8114f4d158a9c10bb7ebb6ddf7f9dbf3fa
Kushal <kushalsapkota577@gmail.com>
2025-11-13 18:31:00 +0545

    feat:updated

--- hw/urls.py
+++ hw/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
-from .views import home, create_homework, edit_homework, hw_delete, profile_list, profile_create, profile_edit, profile_delete
+from .views import (
+    home, create_homework, edit_homework, hw_delete,
+    profile_list, profile_create, profile_edit, profile_delete
+)
 
 urlpatterns = [
+    # Homework URLs
     path('', home, name='home'),
-    path('create/', create_homework, name='create_homework'),
-    path('edit/<int:id>/', edit_homework, name='edit_homework'),
-    path('delete/<int:id>/', hw_delete, name='hw_delete'),
+    path('homework/create/', create_homework, name='create_homework'),
+    path('homework/edit/<int:id>/', edit_homework, name='edit_homework'),
+    path('homework/delete/<int:id>/', hw_delete, name='hw_delete'),
 
-
-    #for profile
-    path('', profile_list, name='profile_list'),
-    path('create/', profile_create, name='profile_create'),
-    path('edit/<int:id>/', profile_edit, name='profile_edit'),
-    path('delete/<int:id>/', profile_delete, name='profile_delete'),
+    # Profile URLs
+    path('profiles/', profile_list, name='profile_list'),
+    path('profiles/create/', profile_create, name='profile_create'),
+    path('profiles/edit/<int:id>/', profile_edit, name='profile_edit'),
+    path('profiles/delete/<int:id>/', profile_delete, name='profile_delete'),
 ]
