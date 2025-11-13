9ebbddf82fd0230aba5c13667f713782afc1808c
Kushal <kushalsapkota577@gmail.com>
2025-11-13 18:20:32 +0545

    feat:updated

--- hw/views.py
+++ hw/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Homework
-from .forms import HomeworkForm
+from .models import Homework, Subject,Profile
+from .forms import HomeworkForm,ProfileForm
 
 
 #HOMEWORK SECTION 
@@ -44,4 +44,48 @@ def edit_homework(request, id):
 def hw_delete(request, id):
     homework = Homework.objects.get(id=id)
     homework.delete()
+    return redirect('home')
+
+
+
+
+
+
+#for the profile now 
+def profile_list(request):
+    profiles = Profile.objects.all()
+    context = {
+        "profiles": profiles
+    }
+    return render(request, "profile_list.html", context)
+
+#for creating the profile
+def profile_create(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProfileForm()
+    return render(request, "profile_create.html", {"form": form})
+
+
+#FOR editing the profile
+def profile_edit(request, id):
+    profile = Profile.objects.get(id=id)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST,  instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_list')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, "profile_edit.html", {"form": form})
+
+
+#for delete the profile
+def profile_delete(request, id):
+    profile = Profile.objects.get(id=id)
+    profile.delete()
     return redirect('home')
