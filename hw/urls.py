d8184c482c14b9286da11afca197b4f18a6a4970
Kushal <kushalsapkota577@gmail.com>
2025-11-14 20:07:01 +0545

    feat:updated

--- hw/urls.py
+++ hw/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import (
     home, create_homework, edit_homework, hw_delete,
-    profile_list, profile_create, profile_edit, profile_delete
+    profile_list, profile_create, profile_edit, profile_delete,
+    comment_list, create_comment, edit_comment, comment_delete
 )
 
 urlpatterns = [
@@ -16,4 +17,10 @@ urlpatterns = [
     path('profiles/create/', profile_create, name='profile_create'),
     path('profiles/edit/<int:id>/', profile_edit, name='profile_edit'),
     path('profiles/delete/<int:id>/', profile_delete, name='profile_delete'),
+
+    # Comment URLs
+    path('comments/', comment_list, name='comment_list'),
+    path('comments/create/', create_comment, name='create_comment'),
+    path('comments/edit/<int:id>/', edit_comment, name='edit_comment'),
+    path('comments/delete/<int:id>/', comment_delete, name='comment_delete'),
 ]
